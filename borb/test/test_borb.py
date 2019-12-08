488028fe1a3e615f0a70f6da06bb4adda6924df7
Max Suica <max.suica@gmail.com>
2019-12-07 20:18:41 -0800

    Add test cases: empty lines, \r, """

--- borb/test/test_borb.py
+++ borb/test/test_borb.py
@@ -7,9 +7,14 @@ from borb import Borb
 
 if __name__ == '__main__':
 
-    testString = """testline 1
+    testString_1 = """testline 1
     testline 2
     testline 3"""
+
+    testString_2 = """ Test line 1
+
+    Test line 2
+    """
 
     class TestBorb(TestCase):
         def test_shortstring(self):
@@ -17,6 +22,13 @@ if __name__ == '__main__':
             instance._speak = (
                 lambda _self, i: self.assertEqual(i, 0)).__get__(instance)
             instance.speak()
+
+        def test_empylines(self):
+            Borb(testString_2).speak()
+
+        def test_emptyinit(self):
+            # Test empty init
+            Borb().speak()
 
         def test_clipboard(self):
             # Test clipboard
@@ -26,14 +38,18 @@ if __name__ == '__main__':
             self.assertEqual(b1.text, test)
 
         def test_mediumstring(self):
-            b2 = Borb("test 2:" + testString).speak()
+            b2 = Borb("test 2:" + testString_1).speak()
             self.assertEqual(b2.segs, 3)
+
         def test_whitepspace(self):
             # Test empty string
             Borb("    ").speak()
+        
+        def test_carriagereturn(self):
+            Borb('\r').speak()
 
-        def test_emptyinit(self):
-            # Test empty init
-            Borb().speak()
+        def test_tripldoublequote(self):
+            Borb('"""').speak()
+
 
 main()
