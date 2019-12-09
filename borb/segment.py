d3f374b865cc6776fc2e947495b9395f17a7a1ae
Max Suica <max.suica@gmail.com>
2019-12-09 12:56:22 -0800

    Add greedy segment merge

--- borb/segment.py
+++ borb/segment.py
@@ -1,4 +1,5 @@
 from collections import UserList
+from functools import reduce
 from itertools import chain
 import json
 from pathlib import Path
@@ -9,6 +10,27 @@ print(Path.cwd())
 
 conf = 'borb/borbConfig.json'
 opt = json.loads(Path(conf).read_text())
+
+class Res:
+    def __init__(self):
+        self.count = 0
+        self.current = [""]
+
+    def append(self, next):
+        self.current[-1] += "\n" + next
+        self.count += len(next) + 1
+
+    def add(self, next):
+        self.current += [next]
+        self.count = len(next)
+
+
+def greedyMerge(res, next):
+    if res.count + len(next) < 3000:
+        res.append(next)
+    else:
+        res.add(next)
+    return res
 
 
 class Segment(UserList):
@@ -26,6 +48,10 @@ class Segment(UserList):
         findall = re.compile(regex).findall
         self.data = cat(map(findall, self.data))
         self.data = [s.strip() for s in self.data]
+        return self
+
+    def merge(self):
+        self.data = reduce(greedyMerge, self.data, Res()).current
         return self
 
     def drop(self, regex="^ *$"):
