a7df9c61e38e0e05e902fd787b5085e597312492
Max Suica <max.suica@gmail.com>
2019-12-09 12:45:40 -0800

    Refactor segment:
    support  init list
    simplify .split
    pep8 format

--- borb/segment.py
+++ borb/segment.py
@@ -1,31 +1,39 @@
 from collections import UserList
 from itertools import chain
-cat = chain.from_iterable
-import json, re
+import json
 from pathlib import Path
+import re
+
+cat = chain.from_iterable
 print(Path.cwd())
+
 conf = 'borb/borbConfig.json'
-opt = json.loads(Path(conf).resolve().read_text())
-CHUNK = opt['chunk']
+opt = json.loads(Path(conf).read_text())
+
 
 class Segment(UserList):
     """Segments text for speech by polly
     """
-    def __init__(self, text):
-        self.data = [text]
+
+    def __init__(self, text='', data=[]):
+        if data:
+            self.data = list(data)
+        else:
+            self.data = [text]
 
     def split(self, chunk):
-        split = re.compile(f".{{1,{chunk}}}").findall
-        self.data = map(lambda s: s.strip(),
-                        cat(map(split, self.data)))
+        regex = f".{{1,{chunk}}}"
+        findall = re.compile(regex).findall
+        self.data = cat(map(findall, self.data))
+        self.data = [s.strip() for s in self.data]
         return self
 
     def drop(self, regex="^ *$"):
-    # default: drop empty segments
-        test = lambda x: not re.match(regex, x)
+        # default: drop empty segments
+        def test(x): return not re.match(regex, x)
         self.data = list(filter(test, self.data))
         return self
-    
+
     @staticmethod
     def normalize(text):
-        return Segment(text).split(CHUNK).drop()
+        return Segment(text).split(opt['chunk']).drop().merge()
